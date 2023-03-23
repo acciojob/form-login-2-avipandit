841e9cc9c92aba5f393694d308458e2c35ef8bd3
AVINASH KUMAR <65335071+avipandit@users.noreply.github.com>
2023-03-23 18:06:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+function display(){
+	let firstname=document.getElementById("fname").value;
+	let lastname=document.getElementById("lname").value;
+	let phonenumber=document.getElementById("phone").value;
+	let email=document.getElementById("email").value;
+
+	alert("FIRST NAME:"+firstname+"\n"+"LAST NAME:"+lastname+"\n"+"PHONE NUMBER:"+phonenumber+"\n"+"Email ID:"+email);
+}
